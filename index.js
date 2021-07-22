640efb9757faac0d285b0c0cc778650a3ffd214c
Alex Parra <alexparra@berkeley.edu>
2021-07-22 09:28:38 -0700

    Set up express server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello world!');
+const express = require('express');
+const app = express();
+ 
+app.get('/result', function (req, res) {
+    res.send('Here is your result: you are 70% of this.');
+  });
+
+app.get('/', function (req, res) {
+  res.send('This is the Purity Test Generator Project.');
+});
+ 
+app.listen(3000);
