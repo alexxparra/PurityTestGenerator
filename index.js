69de1c80e39fc992ec49a4b4da2286cbb625a908
Satwika Paul <satwikapaul@MB.local>
2021-08-01 22:13:58 -0500

    8/1 commit

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 const express = require('express');
 const app = express();
- 
-app.get('/result', function (req, res) {
-    res.send('Here is your result: you are 70% of this.');
+//app.use(express.json());
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.json());
+
+app.get('/homepage', (req, res) => {
+    res.sendFile(__dirname + "/homepage.html");
+});
+
+app.post("/createForm",(req, res) => {
+
+    res.sendFile(__dirname + "/puritypage2.html");
+});
+
+app.get('/createForm', (req, res) => {
+    res.sendFile(__dirname + "/puritypage2.html");
+});
+
+app.post("/helloWorld", (req, res) => {
+
+    res.send("Hello, " + req.body.title);
+    res.send(req.body);
+});
+
+app.get('/yourForm', function (req, res) {
+    res.sendFile(__dirname + "/purityreturnform.html");
+});
+
+app.get('/results', function (req, res) {
+    res.sendFile(__dirname + "/purityreturnresults.html");
   });
 
-app.get('/', function (req, res) {
-  res.send('This is the Purity Test Generator Project.');
-});
  
 app.listen(3000);
 console.log('Server started at http://localhost:3000');
 console.log('CTRL + C to stop the server.');
+
