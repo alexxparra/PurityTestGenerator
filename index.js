666b6c974f69f4718ffbe26355f3961a790c83e9
Alex Parra <alexparra@berkeley.edu>
2021-08-04 10:52:50 -0700

    Added dynamic pages

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+let ejs = require('ejs');
 const app = express();
 //app.use(express.json());
 var bodyParser = require('body-parser');
@@ -9,9 +10,23 @@ app.get('/homepage', (req, res) => {
     res.sendFile(__dirname + "/homepage.html");
 });
 
-app.post("/createForm",(req, res) => {
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/homepage.html");
+});
 
-    res.sendFile(__dirname + "/puritypage2.html");
+app.post("/createForm",(req, res) => {
+    console.log(req.body);
+
+// let people = ['geddy', 'neil', 'alex'];
+// let html = ejs.render('<%= people.join(", "); %>', {people: people});
+
+    for (question in req.body["questions[]"]) {
+        console.log(req.body["questions[]"][question]);
+    }
+    // This following line is how we use a .ejs file to render dynamic data we pass in.
+    ejs.renderFile('./views/purityreturnform.ejs', {questions: req.body["questions[]"]}, {}, (err, str) => {
+        res.send(str);
+    });
 });
 
 app.get('/createForm', (req, res) => {
@@ -26,6 +41,14 @@ app.post("/helloWorld", (req, res) => {
 
 app.get('/yourForm', function (req, res) {
     res.sendFile(__dirname + "/purityreturnform.html");
+});
+
+app.post('/results', function (req, res) {
+    console.log(req.body);
+    var totalQuestions = parseInt(req.body.totalQuestions);
+    var allAnswers = req.body["questionAnswer[]"];
+    var checkedNumber = allAnswers.length;
+    res.send("You got " + (checkedNumber / totalQuestions) + "% correct.");
 });
 
 app.get('/results', function (req, res) {
